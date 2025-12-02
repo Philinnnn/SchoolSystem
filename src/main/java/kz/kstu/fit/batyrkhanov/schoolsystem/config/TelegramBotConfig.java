3cb32611e6ab15caf2dfbe1f29553a46a1a5c0e3
Armando <159049463+Philinnnn@users.noreply.github.com>
2025-12-02 23:39:21 +0500

    TelegramBotConfig deleted

--- src/main/java/kz/kstu/fit/batyrkhanov/schoolsystem/config/TelegramBotConfig.java
+++
@@ -1,9 +0,0 @@
-package kz.kstu.fit.batyrkhanov.schoolsystem.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TelegramBotConfig {
-    // Перенесено в отдельный компонент TelegramRuntimeConfig.
-}
-
